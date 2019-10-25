a33e47583aaef1e34fbf5816ed3c010cef612fb2
Milos Mrdovic <shellexecute@gmail.com>
2019-10-25 01:05:08 +0100

    Add sensor fusion (6-axis) test script.

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+sudo apt install python3-numpy
 git clone https://github.com/MomsFriendlyRobotCompany/mpu9250.git
 git clone https://github.com/pimoroni/bmp280-python
---
+++ sensor-fusion-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('./mpu9250/mpu9250')
+
+from mpu9250 import mpu9250
+from ahrs.filters import Madgwick
+from ahrs.common import DEG2RAD
+from time import sleep
+import math
+
+try:
+    imu = mpu9250()
+    mw = Madgwick(frequency = 10)
+    q = [1, 0, 0, 0]
+    while True:
+        a = list(imu.accel)
+        g = list(map(lambda x: x*DEG2RAD, imu.gyro))
+        m = list(imu.mag)
+        q = mw.updateIMU(g, a, q)
+        print('w:{:.3f} x:{:.3f} y:{:.3f} z:{:.3f}'.format(*(q)))
+        sleep(0.1)
+except KeyboardInterrupt:
+    print('bye ...')
