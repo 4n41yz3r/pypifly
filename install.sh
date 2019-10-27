c9130709482bef0d65f2db76c6cfaa8b344ed779
Milos Mrdovic <shellexecute@gmail.com>
2019-10-27 21:17:30 +0000

    Update install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt install python3-numpy
 git clone https://github.com/MomsFriendlyRobotCompany/mpu9250.git
-git clone https://github.com/pimoroni/bmp280-python
+git clone https://github.com/pimoroni/bmp280-python.git
+git clone https://github.com/morgil/madgwick_py.git
