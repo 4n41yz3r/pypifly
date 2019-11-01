d0ae112707669380fead294385835536c9229cd3
Milos Mrdovic <shellexecute@gmail.com>
2019-11-01 20:43:40 +0000

    Improve sensor fusion 2 script.

--- sensor_fusion2.py
+++ sensor_fusion2.py
@@ -5,28 +5,37 @@ sys.path.append('./mpu9250/mpu9250')
 
 from mpu9250 import mpu9250
 from madgwick_py.madgwickahrs import MadgwickAHRS
+from madgwick_py.quaternion import Quaternion
 from ahrs.filters import Madgwick
 from ahrs.common import DEG2RAD
 from ahrs.common import RAD2DEG
 from ahrs.common.orientation import q2euler
+from calibrator import DynamicCompassCalibrator
 from time import sleep
 import math
 
 def rad2deg(a):
     return list(map(lambda x: x*RAD2DEG, a))
 
+def show3(v):
+    print('x:{:+10.5f}'.format(v[0]), end=' ')
+    print('y:{:+10.5f}'.format(v[1]), end=' ')
+    print('z:{:+10.5f}'.format(v[2]))
+
 try:
     imu = mpu9250()
-    mw = MadgwickAHRS(0.02)
+    mw = MadgwickAHRS(0.02, Quaternion(1, 0, 0, 0))
+    dcc = DynamicCompassCalibrator()
     while True:
-        a = list(imu.accel)
+        a = imu.accel
         g = list(map(lambda x: x*DEG2RAD, imu.gyro))
-        m = list(imu.mag)
+        #m = list(imu.mag)
+        #m = dcc.correct(imu.mag)
         mw.update_imu(g, a)
         #mw.update(g, a, m)
         #print('w:{:.3f} x:{:.3f} y:{:.3f} z:{:.3f}'.format(*mw.quaternion))
-        a = mw.quaternion.to_euler_angles()
-        print('x:{:.3f} y:{:.3f} z:{:.3f}'.format(*rad2deg(a)))
+        ea = mw.quaternion.to_euler_angles()
+        show3(rad2deg(ea))
         sleep(0.02)
 except KeyboardInterrupt:
     print('bye ...')
